15b18f76f4766b6948e521be6c6a17241d04a22b
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-05-30 23:17:28 -0300

    chore: add Tests - Tesults pipeline

---
+++ runner.js
@@ -0,0 +1,14 @@
+const cypress = require('cypress')
+const tesults = require('cypress-tesults-reporter')
+
+cypress.run({
+})
+  .then((results) => {
+    const args = {
+      target: process.env.TESULTS_TOKEN,
+    }
+    tesults.results(results, args)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
